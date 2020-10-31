978873099307fee42a2a65d77b64e9d8baff089c
Clive Gurure <clivethescott@gmail.com>
2020-10-31 18:00:32 +0200

    updates to backup script

--- backup_dotfiles.sh
+++ backup_dotfiles.sh
@@ -1,9 +1,11 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env sh
 
 DOTFILES_HOME=$HOME/dotfiles
+cp -r $HOME/.vim/after/ftplugin $DOTFILES_HOME/vim/after/ftplugin
 cp -r $HOME/.vim/vimrc $HOME/.vim/after $DOTFILES_HOME/vim
 cp -r $HOME/.gitconfig $HOME/.gitignore_global $DOTFILES_HOME/git
-cp -r $HOME/.config/nvim/*.{vim,json} $HOME/.config/nvim/UltiSnips $DOTFILES_HOME/nvim
+cp -r $HOME/.config/nvim/*.vim $HOME/.config/nvim/UltiSnips $DOTFILES_HOME/nvim
+cp -r $HOME/.config/nvim/coc-settings.json $DOTFILES_HOME/nvim
 cp $HOME/.ideavimrc $DOTFILES_HOME/ideavim
 cp $HOME/.ripgreprc $DOTFILES_HOME/ripgrep
 cp $HOME/.tmux.conf $DOTFILES_HOME/tmux
