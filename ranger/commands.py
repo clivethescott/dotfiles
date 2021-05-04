6099c5faf3fb62c39215526eb95ff841906092d2
Clive Gurure <clivethescott@gmail.com>
2021-05-04 22:20:15 +0200

    update i3 config + compress for ranger

--- ranger/commands.py
+++ ranger/commands.py
@@ -10,9 +10,6 @@
 from __future__ import (absolute_import, division, print_function)
 # You can import any python module as needed.
 import os
-
-from utils import subs
-
 # You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
 
@@ -66,20 +63,53 @@ class duplicate(Command):
         # content of the current directory.
         return self._tab_directory_content()
 
-# Any class that is a subclass of "Command" will be integrated into ranger as a
-# command.  Try typing ":my_edit<ENTER>" in ranger!
 class subcp(Command):
     """:subtitle_copy
 
     Locate + copy subtitle for current video file
     """
 
-    # The execute method is called when you run this command in ranger.
     def execute(self):
+        from utils import subs
         subs.cp(self.fm.thisfile.path)
         
     def tab(self, _):
-        # This is a generic tab-completion function that iterates through the
-        # content of the current directory.
         return self._tab_directory_content()
+
+class compress(Command):
+    def execute(self):
+        """:compress archivename
+
+        Compress marked files to current directory using atool
+        """
+
+        from ranger.core.loader import CommandLoader
+
+        cwd = self.fm.thisdir
+        marked_files = cwd.get_selection()
+
+        if not marked_files:
+            self.fm.notify("No files to compress", bad=True)
+            return
+
+        def reload_dir(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        original_path = cwd.path
+        parts = self.line.split()
+        au_flags = parts[1:]
+
+        descr = "compressing files in: " + os.path.basename(parts[1])
+        obj = CommandLoader(args=['apack'] + au_flags + \
+                [os.path.relpath(f.path, cwd.path) for f in marked_files], descr=descr)
+
+        obj.signal_bind('after', reload_dir)
+        self.fm.loader.add(obj)
+
+    def tab(self):
+        """ Complete with current folder name """
+
+        extension = ['.zip', '.tar.gz', '.rar', '.7z']
+        return ['compress ' + os.path.basename(self.fm.thisdir.path) + ext for ext in extension]
 
