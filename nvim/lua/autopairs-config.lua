f265b246b8bc92b8c9cc45c77af55b5c21c259b6
Clive Gurure <clivethescott@gmail.com>
2022-04-26 00:06:01 +0200

    triple quoted string auto pair

--- nvim/lua/autopairs-config.lua
+++ nvim/lua/autopairs-config.lua
@@ -23,3 +23,12 @@ require('nvim-autopairs').setup({
 local cmp_autopairs = require('nvim-autopairs.completion.cmp')
 local cmp = require('cmp')
 cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
+
+-- Apply custom rules
+local Rule = require('nvim-autopairs.rule')
+local npairs = require('nvim-autopairs')
+-- local cond = require('nvim-autopairs.conds')
+
+npairs.add_rules({
+  Rule('"""', '"""', { "scala", "java", "python" }), -- triple quoted strings
+})
