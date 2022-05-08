1573aec7538795d8a71ce828062308085c23cd6a
Clive Gurure <clivethescott@gmail.com>
2022-05-08 15:37:54 +0200

    add keyword length to cmp sources

--- nvim/lua/lsp/cmp.lua
+++ nvim/lua/lsp/cmp.lua
@@ -73,12 +73,12 @@ M.setup = function()
     sources = cmp.config.sources({
       { name = 'path', max_item_count = 3 },
     }, {
-      { name = 'nvim_lua', max_item_count = 5 },
-      { name = 'nvim_lsp', max_item_count = 15 },
-      { name = 'luasnip', max_item_count = 5 },
-      { name = 'nvim_lsp_document_symbol', max_item_count = 10 },
+      { name = 'nvim_lua', max_item_count = 5, keyword_length = 3 },
+      { name = 'nvim_lsp', max_item_count = 15, keyword_length = 3 },
+      { name = 'luasnip', max_item_count = 5, keyword_length = 2 },
+      { name = 'nvim_lsp_document_symbol', max_item_count = 10, keyword_length = 3 },
       { name = 'nvim_lsp_signature_help' },
-      { name = 'buffer', max_item_count = 5 },
+      { name = 'buffer', max_item_count = 5, keyword_length = 3 },
     }, {
       { name = 'calc' },
     })
