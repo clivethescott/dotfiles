b9aa1dd4018bc093e7f54f0abe3f4d83b1e0aaaa
Clive Gurure <clivethescott@gmail.com>
2022-05-09 15:46:40 +0200

    more contrast on tokyonight theme line numbers

--- nvim/lua/plug/colors.lua
+++ nvim/lua/plug/colors.lua
@@ -7,3 +7,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+if vim.g.colors_name == 'tokyonight' then
+  -- More contrast for Line numbers when using tokyonight
+  vim.cmd [[highlight LineNr cterm=underline ctermfg=11 guifg=#737aa2 ]]
+end
