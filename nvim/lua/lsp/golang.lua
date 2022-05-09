121da677bfb6e03f7bfcfb9ce1930ec23fcccd90
Clive Gurure <clivethescott@gmail.com>
2022-05-09 15:45:06 +0200

    additional LSP settings for gopls

--- nvim/lua/lsp/golang.lua
+++ nvim/lua/lsp/golang.lua
@@ -6,17 +6,34 @@ M.setup = function(on_attach, capabilities)
   end
 
   lspconfig.gopls.setup {
-    capabilities = capabilities;
-    filetypes = { 'go' };
+    capabilities = capabilities,
+    filetypes = { 'go' },
     on_attach = function(client, bufnr)
       -- These will be handled by null-ls goimports
       client.server_capabilities.documentFormattingProvider = false
       client.server_capabilities.documentRangeFormattingProvider = false
       on_attach(client, bufnr)
-    end;
+    end,
+    settings = {
+      gopls = {
+        experimentalPostfixCompletions = true,
+        analyses = {
+          unusedparams = true,
+          shadow = true,
+        },
+        staticcheck = true,
+      },
+    },
+    init_options = {
+      usePlaceholders = true,
+    },
   }
 
-  lspconfig.golangci_lint_ls.setup{}
+  lspconfig.golangci_lint_ls.setup {
+    capabilities = capabilities,
+    filetypes = { 'go' },
+    on_attach = on_attach,
+  }
 end
 
 return M
