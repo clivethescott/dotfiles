ab65ef46ed76aa23ab13570ef4abe5f7564cf93a
Clive Gurure <clivethescott@gmail.com>
2022-05-15 12:10:55 +0200

    enable normal mode in dressing's vim.ui.input
    This will allow easier navigation via use of normal mode keybinds

--- nvim/lua/plug/dressing.lua
+++ nvim/lua/plug/dressing.lua
@@ -15,7 +15,7 @@ dressing.setup({
     prompt_align = "center",
 
     -- When true, <Esc> will close the modal
-    insert_only = true,
+    insert_only = false,
 
     -- These are passed to nvim_open_win
     anchor = "SW",
