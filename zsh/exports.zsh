2adb3e92ffdb17c2dd73e0f6967b18310720c8d5
Clive Gurure <clivethescott@gmail.com>
2022-05-15 21:25:25 +0200

    use rust tools via homebrew

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -3,7 +3,6 @@ export MYVIMRC=$HOME/.vim/vimrc
 export APP_HOME=$HOME/apps
 export VISUALVM_HOME=$APP_HOME/visualvm
 export IDEA_HOME=$APP_HOME/intellij
-export CARGO_HOME=$HOME/.cargo
 export EDITOR=nvim
 export VISUAL=$EDITOR
 export HOSTALIASES=$HOME/.hosts
@@ -23,7 +22,7 @@ export SUBLIME_HOME='/Applications/Sublime Text.app/Contents/SharedSupport/bin'
 export JAVA_HOME=$(/usr/libexec/java_home -v 17)
 export PATH=$JAVA_HOME/bin:$SBT_HOME/bin:$COURSIER_HOME/bin:$PATH
 # Other programming tools
-export PATH=$GO_HOME/bin:$PROTOBUF_HOME/bin:$GOBIN:$CARGO_HOME/bin:$APP_HOME/selenium:$VISUALVM_HOME/bin:$HOME/local/bin:$PIP_TOOLS/bin:$PATH
+export PATH=$GO_HOME/bin:$PROTOBUF_HOME/bin:$GOBIN:$APP_HOME/selenium:$VISUALVM_HOME/bin:$HOME/local/bin:$PIP_TOOLS/bin:$PATH
 export PATH=$REDIS_HOME/bin:$IDEA_HOME/bin:$PATH
 # Setting rg as the default source for fzf
 # export FZF_DEFAULT_COMMAND='rg --files --vimgrep'
