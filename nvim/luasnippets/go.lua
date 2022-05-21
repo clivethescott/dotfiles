22c487717ce6a37d828d61808d5f942f41f0f564
Clive Gurure <clivethescott@gmail.com>
2022-05-21 22:40:39 +0200

    add go snippets

--- nvim/luasnippets/go.lua
+++ nvim/luasnippets/go.lua
@@ -3,11 +3,27 @@ return {
     t({ "if err != nil {", "\t" }),
     i(1),
     t({ "\t", "}" }),
-  }),
-  s("gof", fmt([[
-  go func() {{
+  }
+  ),
+  s({trig='gof', dscr = 'Runs func in a goroutine'}, fmt([[
+    go func() {{
+      {}
+    }}()
     {}
-  }}()
-  {}
-  ]], { i(1), i(0)}))
+  ]], { i(1), i(0) })
+  ),
+  s("ifg", fmt([[
+    if got != want {{
+      t.Errorf("got %{}, want %{}", got, want)
+    }}
+    {}
+  ]], { c(1, { t "v", t "q", t "s", t "d" }), rep(1), i(0) })
+  ),
+  s({trig='subt', dscr='Create a sub test'}, fmt([[
+    t.Run("{}", func(t *testing.T) {{
+      {}
+    }})
+  ]], { i(1), i(0)})
+  ),
 }
+
