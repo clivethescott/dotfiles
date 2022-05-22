5a1ca2f54546a2c678e7ac60d617e40138893a2f
Clive Gurure <clivethescott@gmail.com>
2022-05-22 18:48:06 +0200

    config line number highlight using Lua

--- nvim/lua/plug/colors.lua
+++ nvim/lua/plug/colors.lua
@@ -10,9 +10,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
-if vim.g.colors_name == 'tokyonight' then
   -- More contrast for Line numbers when using tokyonight
-  vim.cmd [[highlight LineNr cterm=underline ctermfg=11 guifg=#737aa2 ]]
+if vim.g.colors_name == 'tokyonight' then
+  vim.api.nvim_set_hl(0, 'LineNr', { fg = '#737aa2'})
 end
 vim.api.nvim_set_hl(0, 'WinBarPath', { fg = '#545c7e' })
 vim.api.nvim_set_hl(0, 'WinBarModified', { fg = '#e0af68' })
