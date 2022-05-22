847537660731ef0b1cb78dc01f06986dfc2edf13
Clive Gurure <clivethescott@gmail.com>
2022-05-22 18:47:33 +0200

    increase yank highlight duration

--- nvim/lua/plug/colors.lua
+++ nvim/lua/plug/colors.lua
@@ -2,7 +2,9 @@
 local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   callback = function()
-    vim.highlight.on_yank()
+    vim.highlight.on_yank {
+      timeout = 500 -- time in ms before highlight is cleared
+    }
   end,
   group = highlight_group,
   pattern = '*',
