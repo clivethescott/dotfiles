3dc7e4e1d87b877c0bae8393976ad538169d96a8
Clive Gurure <clivethescott@gmail.com>
2022-06-05 22:21:23 +0200

    recommended config of lua lang server

--- nvim/lua/lsp/luaserver.lua
+++ nvim/lua/lsp/luaserver.lua
@@ -5,15 +5,41 @@ M.setup = function(on_attach, capabilities)
     return
   end
 
-  local root_pattern = lspconfig.util.root_pattern
   local runtime_path = vim.split(package.path, ';')
   table.insert(runtime_path, 'lua/?.lua')
   table.insert(runtime_path, 'lua/?/init.lua')
 
+  local library = {}
+
+  local function add(lib)
+    for _, p in pairs(vim.fn.expand(lib, false, true)) do
+      p = vim.loop.fs_realpath(p)
+      library[p] = true
+    end
+  end
+
+  -- add runtime
+  add("$VIMRUNTIME")
+
+  -- add your config
+  add("~/.config/nvim")
+
+  -- add plugins
+  -- if you're not using packer, then you might need to change the paths below
+
+  add("~/.local/share/nvim/site/pack/packer/opt/*")
+  add("~/.local/share/nvim/site/pack/packer/start/*")
+
   lspconfig.sumneko_lua.setup {
+    -- delete root from workspace to make sure we don't trigger duplicate warnings
+    on_new_config = function(config, root)
+      local libs = vim.tbl_deep_extend("force", {}, library)
+      libs[root] = nil
+      config.settings.Lua.workspace.library = libs
+      return config
+    end,
     on_attach = on_attach,
     capabilities = capabilities,
-    root_dir = root_pattern(".luarc.json", ".luacheckrc", ".stylua.toml", "selene.toml", ".git"),
     settings = {
       Lua = {
         runtime = {
@@ -29,7 +55,9 @@ M.setup = function(on_attach, capabilities)
         },
         workspace = {
           -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file('', true),
+          library = library,
+          maxPreload = 2000,
+          preloadFileSize = 50000,
         },
         telemetry = {
           enable = false,
