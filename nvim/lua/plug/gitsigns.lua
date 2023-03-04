4d443f0e962251ea9adb56df6ac0960f8023a6d3
Clive Gurure <clivethescott@gmail.com>
2023-03-04 18:06:09 +0000

    disable highlighting in gitsigns

--- nvim/lua/plug/gitsigns.lua
+++ nvim/lua/plug/gitsigns.lua
@@ -38,8 +38,8 @@ signs = {
   changedelete = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
 },
 signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
+numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
 watch_gitdir = {
   interval = 1000,
   follow_files = true
