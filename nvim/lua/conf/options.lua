b69b6614a55445399766692ac466444ed6c209f4
Clive Gurure <clivethescott@gmail.com>
2023-10-22 22:30:21 +0100

    disable remote plugins

--- nvim/lua/conf/options.lua
+++ nvim/lua/conf/options.lua
@@ -81,11 +81,12 @@ vim.opt.smarttab = true
 -- Continue indent on new line
 vim.opt.smartindent = true
 
--- Nvim config
+-- Disable remote plugins
 vim.g.loaded_python_provider = 0
 vim.g.loaded_perl_provider = 0
-vim.g.python3_host_prog = '/opt/homebrew/opt/python@3.11/bin/python3'
+vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
+vim.g.loaded_node_provider = 0
 
 vim.opt.foldmethod = 'syntax'
 vim.opt.foldminlines = 20
