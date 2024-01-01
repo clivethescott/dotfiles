0b5991fa03e42b7c96c02f9cc93485f8186bee03
Clive Gurure <clivethescott@gmail.com>
2024-01-01 23:20:16 +0000

    reorder snippets and lsp in cmp completion

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -110,8 +110,8 @@ return {
       sources = cmp.config.sources({
         { name = 'path', max_item_count = 30 },
       }, {
-        { name = 'luasnip',                  max_item_count = 3, keyword_length = 1 },
         { name = 'nvim_lsp',                 max_item_count = 25 },
+        { name = 'luasnip',                  max_item_count = 3, keyword_length = 1 },
         { name = 'cmp_tabnine',              max_item_count = 5, keyword_length = 3 },
         { name = 'nvim_lua',                 max_item_count = 5 },
         -- { name = 'nvim_lsp_signature_help' }, -- now using lsp_signature
