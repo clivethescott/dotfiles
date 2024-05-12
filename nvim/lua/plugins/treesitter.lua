64d44bdd1c428453e731b4a6ed24fe1468589b44
Clive Gurure <clivethescott@gmail.com>
2024-05-12 10:51:13 +0100

    remove ts context plugin

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -5,6 +5,13 @@ return {
     pcall(require('nvim-treesitter.install').update { with_sync = false })
   end,
   dependencies = {
+    { 'nvim-treesitter/nvim-treesitter-textobjects', event = 'InsertEnter' },
+    {
+      'windwp/nvim-ts-autotag',
+      ft = { 'html' },
+      event = 'InsertEnter',
+    },
+    { 'nvim-treesitter/playground',                  cmd = 'TSPlaygroundToggle' }
   },
   config = function()
     local treesitter_config = require 'nvim-treesitter.configs'
