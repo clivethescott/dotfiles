0e3276e7d99fda629f7185b634cedb10b86b9161
Clive Gurure <clivethescott@gmail.com>
2024-06-01 17:26:25 +0100

    remove unused plugins

--- nvim/lua/plugins/tokyonight.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/tokyonight.nvim",
-  lazy = false,  -- make sure we load this during startup if it is your main colorscheme
-  priority = 1000,
-  cond = false,
-  config = function()
-    vim.cmd([[colorscheme tokyonight-moon]])
-  end,
-}
