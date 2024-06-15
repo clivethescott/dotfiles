8b81e575b0ba8d96f3eecfc71f0502baeca51038
Clive Gurure <clivethescott@gmail.com>
2024-06-15 23:04:45 +0100

    update wezterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,61 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local mux = wezterm.mux
+local action = wezterm.action
+
+local cmd_bindings = function()
+  local all_characters = [[`1234567890=qwertyuiop[]\asdfghjkl;'zxcbnm./^*$#|?!]]
+  local chars = {}
+
+  -- convert string to list
+  _ = all_characters:gsub(".", function(char) table.insert(chars, char) end)
+  local result = {
+    {
+      key = 'v',
+      mods = 'CMD',
+      action = action.PasteFrom 'Clipboard',
+    },
+    {
+      key = '-',
+      mods = 'CMD',
+      action = action.DecreaseFontSize
+    },
+    {
+      key = '+',
+      mods = 'CMD',
+      action = action.IncreaseFontSize
+    },
+    {
+      key = 'q',
+      mods = 'CMD|OPT',
+      action = action.QuitApplication
+    },
+  }
+
+  for _, char in ipairs(chars) do
+    table.insert(result, {
+      key = char,
+      mods = 'CMD',
+      action = action.SendKey { key = char, mods = 'CTRL' },
+    })
+  end
+
+  return result
+end
+
+-- maximise window on startup
+wezterm.on('gui-startup', function(cmd)
+  local _, _, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
+return {
+  term = 'wezterm',
+  keys = cmd_bindings(),
+  hide_tab_bar_if_only_one_tab = true,
+  font_size = 15,
+  font = wezterm.font 'FiraCode Nerd Font',
+  color_scheme = 'catppuccin-mocha',
+  window_decorations = "RESIZE",
+  audible_bell = "Disabled",
+}
