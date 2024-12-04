ae40f3f9395e21a484e0de41ef4a505d43b2a77a
Clive Gurure <clive.gurure@disneystreaming.com>
2024-12-04 17:13:48 +0000

    add autocmd for yank highlight

--- nvim/lua/conf/autocommands.lua
+++ nvim/lua/conf/autocommands.lua
@@ -133,3 +133,16 @@ api.nvim_create_autocmd({ 'InsertEnter' }, {
     vim.opt.conceallevel = 0
   end
 })
+
+
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank {
+      higroup = 'TermCursor',
+      timeout = 500 -- time in ms before highlight is cleared
+    }
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
