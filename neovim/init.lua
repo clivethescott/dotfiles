146015aa91cea3c91b705d94a205e2a7a0963881
Clive Gurure <clive.gurure@disneystreaming.com>
2024-12-21 14:11:07 +0000

    setup new neovim

---
+++ neovim/init.lua
@@ -0,0 +1,37 @@
+vim.g.mapleader = ','
+
+-- Setup lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require 'lazy'.setup({
+  { import = "plugins" }
+}, {
+  lockfile = vim.fn.stdpath("data") .. "/lazy/lazy-lock-2.json",
+  change_detection = {
+    notify = false
+  },
+  rocks = {
+    enabled = false,
+  },
+  checker = {
+    enabled = false,
+    notify = true,                -- get a notification when new updates are found
+    frequency = 60 * 60 * 24 * 7, -- check for updates every week
+  },
+  performance = {
+    disabled_plugins = {
+      'gzip', 'netrwPlugin', 'tarPlugin', 'tohtml', 'tutor', 'zipPlugin'
+    }
+  }
+})
---
+++ neovim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,61 @@
+return {
+  "ibhagwan/fzf-lua",
+  event = 'VeryLazy',
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("fzf-lua").setup({
+      "telescope",
+      winopts = {
+        height = 0.9,
+        width = 0.9,
+        preview = {
+          default = 'bat',
+          hidden = 'hidden'
+        }
+      },
+    })
+    require("fzf-lua").register_ui_select()
+
+
+    vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>",
+      function() require("fzf-lua").complete_path() end,
+      { silent = true, desc = "Fuzzy complete path" })
+
+    vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>",
+      function() require("fzf-lua").lines() end,
+      { silent = true, desc = "Buffer lines" })
+
+    vim.keymap.set({ "n", "v", "i" }, "<C-x>s",
+      function() require("fzf-lua").spell_suggest() end,
+      { silent = true, desc = "Spell suggest" })
+
+    vim.keymap.set("n", "<leader>2",
+      function() require("fzf-lua").files { cwd = '~/.config/nvim' } end,
+      { silent = true, desc = "Nvim files" })
+
+    local plugins_dir = vim.fs.joinpath(vim.fn.stdpath('data'), '/lazy')
+    vim.keymap.set("n", "<leader>5",
+      function()
+        require("fzf-lua").live_grep_glob { cwd = plugins_dir }
+      end,
+      { silent = true, desc = "Nvim files" })
+  end,
+  keys = {
+    { "<c-p>",     "<cmd>FzfLua files<cr>",          desc = "FZF files" },
+    { "<c-e>",     "<cmd>FzfLua buffers<cr>",        desc = "FZF buffers" },
+    { "<space>tl", "<cmd>FzfLua resume<cr>",         desc = "FZF resume" },
+    { "<space>tq", "<cmd>FzfLua quickfix<cr>",       desc = "FZF quickfix" },
+    { "<space>tf", "<cmd>FzfLua live_grep_glob<cr>", desc = "FZF live grep" },
+    { "<space>tF", "<cmd>FzfLua grep_cword<cr>",     desc = "FZF grep cword" },
+    { "<space>tc", "<cmd>FzfLua commands<cr>",       desc = "FZF commands" },
+    { "<space>th", "<cmd>FzfLua helptags<cr>",       desc = "FZF helptags" },
+    { "<space>tm", "<cmd>FzfLua marks<cr>",          desc = "FZF marks" },
+    { "<space>tr", "<cmd>FzfLua registers<cr>",      desc = "FZF registers" },
+    { "<space>tb", "<cmd>FzfLua tmux_buffers<cr>",   desc = "FZF tmux paste" },
+    {
+      "<space>to",
+      function() require 'fzf-lua'.oldfiles { include_current_session = true } end,
+      desc = "FZF old files"
+    },
+  },
+}
