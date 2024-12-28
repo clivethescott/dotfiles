b18b9c6a19e7acf81e3cf2c0c029ff63a1127476
Clive Gurure <clivethescott@gmail.com>
2024-12-28 16:41:01 +0000

    blink doc window keybinds

--- neovim/lua/deps/blink-cmp.lua
+++ neovim/lua/deps/blink-cmp.lua
@@ -14,7 +14,8 @@ return {
       ['<C-y>'] = { 'select_and_accept' },
       ['<c-j>'] = { 'snippet_forward', 'fallback' },
       ['<c-k>'] = { 'snippet_backward', 'fallback' },
-      -- ['<c-g>'] = { function() require 'blink.cmp'.show_documentation() end },
+      ['<c-f>'] = { 'scroll_documentation_up', 'fallback' },
+      ['<c-b>'] = { 'scroll_documentation_down', 'fallback' },
       cmdline = {
         preset = 'enter',
         ['<tab>'] = { 'select_next' },
