543271b305e26253ac18679ec9f3b684e718bf0a
Clive Gurure <clivethescott@gmail.com>
2024-12-31 23:19:20 +0000

    add custom filetypes

---
+++ neovim/filetype.lua
@@ -0,0 +1,13 @@
+vim.filetype.add({
+  extension = {
+    hcl = "hcl",
+    terraformrc = "hcl",
+    tf = "terraform",
+    sc = "scala",
+    tfvars = "terraform",
+    tfstate = "json",
+    pp = "json",
+    sql = "sql",
+    hurl = "hurl",
+  },
+})
