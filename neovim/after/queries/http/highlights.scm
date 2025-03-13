9600c2a499559b771a6ca3db0cb5ea0c57ade8b7
Clive Gurure <clivethescott@gmail.com>
2025-03-13 16:46:16 +0000

    update http treesitter highlights

--- neovim/after/queries/http/highlights.scm
+++ neovim/after/queries/http/highlights.scm
@@ -1,9 +1,17 @@
 ; extends
 
+; https://github.com/nvim-treesitter/nvim-treesitter/blob/master/queries/http/highlights.scm
+
 (request
   (raw_body) @request.body)
 
+(request
+  (target_url) @request.url)
+
 (variable_declaration
+  (identifier) @variable.declaration)
+
+(variable
   (identifier) @variable.identifier)
 
 (variable_declaration
