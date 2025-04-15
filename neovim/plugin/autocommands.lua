ecad33550a1b4717d350599423c38ac8ed5a8e02
Clive Gurure <clivethescott@gmail.com>
2025-04-15 09:54:32 +0100

    autofold imports

--- neovim/plugin/autocommands.lua
+++ neovim/plugin/autocommands.lua
@@ -10,23 +10,33 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
+-- To automatically fold imports when opening a file, see :h vim.lsp.foldclose
+vim.api.nvim_create_autocmd('LspNotify', {
+  group = vim.api.nvim_create_augroup('MyLspNotify', { clear = true }),
+  callback = function(args)
+    if args.data.method == 'textDocument/didOpen' then
+      vim.lsp.foldclose('imports', vim.fn.bufwinid(args.buf))
+    end
+  end,
+})
+
 vim.api.nvim_create_autocmd({ "LspAttach" }, {
-    group = vim.api.nvim_create_augroup('LspConfig', { clear = true }),
-    callback = function(args)
-      local bufnr = args.buf
-      local client = vim.lsp.get_client_by_id(args.data.client_id)
-      if client then
-        -- if client.server_capabilities.inlayHintProvider then
-        --   vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
-        -- end
-        require 'lsp'.on_attach(client, bufnr)
-      end
-    end,
+  group = vim.api.nvim_create_augroup('MyLspAttach', { clear = true }),
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client then
+      -- if client.server_capabilities.inlayHintProvider then
+      --   vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+      -- end
+      require 'lsp'.on_attach(client, bufnr)
+    end
+  end,
 })
 
 -- https://github.com/neovim/neovim/issues/16339#issuecomment-1457394370
 vim.api.nvim_create_autocmd('BufRead', {
-  pattern = {'*.lua', '*.scala', '*.rust', '*.conf', '*.smithy'},
+  pattern = { '*.lua', '*.scala', '*.rust', '*.conf', '*.smithy' },
   callback = function(opts)
     vim.api.nvim_create_autocmd('BufWinEnter', {
       once = true, -- set to false to run this multiple times
@@ -35,9 +45,9 @@ vim.api.nvim_create_autocmd('BufRead', {
         local ft = vim.bo[opts.buf].filetype
         local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
         if
-          not (ft:match('commit') and ft:match('rebase'))
-          and last_known_line > 1
-          and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+            not (ft:match('commit') and ft:match('rebase'))
+            and last_known_line > 1
+            and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
         then
           vim.api.nvim_feedkeys([[g`"]], 'nx', false)
         end
