51d12261db6b80774a1a60e6f39592e7918dd4ce
Clive Gurure <clivethescott@gmail.com>
2025-04-17 13:34:27 +0100

    setup mise treesitter highlights

--- neovim/lua/deps/treesitter.lua
+++ neovim/lua/deps/treesitter.lua
@@ -7,6 +7,14 @@ return {
   dependencies = {
     { 'nvim-treesitter/nvim-treesitter-textobjects', event = 'InsertEnter' },
   },
+  init = function()
+    -- https://mise.jdx.dev/mise-cookbook/neovim.html#code-highlight-for-run-commands
+    require("vim.treesitter.query").add_predicate("is-mise?", function(_, _, bufnr, _)
+      local filepath = vim.api.nvim_buf_get_name(tonumber(bufnr) or 0)
+      local filename = vim.fn.fnamemodify(filepath, ":t")
+      return string.match(filename, ".*mise.*%.toml$")
+    end, { force = true, all = false })
+  end,
   config = function()
     local treesitter_config = require 'nvim-treesitter.configs'
     treesitter_config.setup {
