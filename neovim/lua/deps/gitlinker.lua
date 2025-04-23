64942211ea70457184eb32edbda856dbed03801a
Clive Gurure <clivethescott@gmail.com>
2025-04-23 10:31:32 +0100

    simplify gitlink

--- neovim/lua/deps/gitlinker.lua
+++ neovim/lua/deps/gitlinker.lua
@@ -6,24 +6,14 @@ return {
     { "<space>gy", "<cmd>GitLink<cr>",  mode = { "n", "v" }, desc = "Yank git link" },
     { "<space>gY", "<cmd>GitLink!<cr>", mode = { "n", "v" }, desc = "Open git link" },
   },
-  opts = {
-    router = {
-      browse = {
-        ["^github%.bamtech%.co"] = "https://github.bamtech.co/"
-            .. "{_A.ORG}/"
-            .. "{_A.REPO}/blob/"
-            .. "{_A.REV}/"
-            .. "{_A.FILE}?plain=1"   -- '?plain=1'
-            .. "#L{_A.LSTART}"
-            .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
-        ["^github%.twdcgrid%.net"] = "https://github.twdcgrid.net/"
-            .. "{_A.ORG}/"
-            .. "{_A.REPO}/blob/"
-            .. "{_A.REV}/"
-            .. "{_A.FILE}?plain=1"   -- '?plain=1'
-            .. "#L{_A.LSTART}"
-            .. "{(_A.LEND > _A.LSTART and ('-L' .. _A.LEND) or '')}",
+  config = function()
+    require 'gitlinker'.setup {
+      router = {
+        browse = {
+          ["^github%.bamtech%.co"] = require('gitlinker.routers').github_browse,
+          ["^github%.twdcgrid%.net"] = require('gitlinker.routers').github_browse,
+        }
       }
-    },
-  }
+    }
+  end
 }
