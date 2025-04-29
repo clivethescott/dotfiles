7361e2f3f15d1adde0a136240495a4b62f44c8d3
Clive Gurure <clivethescott@gmail.com>
2025-04-30 00:15:27 +0100

    remove neogit ref

--- neovim/lua/deps/diffview.lua
+++ neovim/lua/deps/diffview.lua
@@ -8,15 +8,6 @@ return {
     default_args = {
       DiffviewOpen = { "--imply-local" }, -- https://github.com/sindrets/diffview.nvim/blob/main/USAGE.md#comparing-all-the-changes
     },
-    keymaps = {
-      file_panel = {
-        {
-          "n", "cc",
-          function() require 'neogit'.open({ 'commit' }) end,
-          { desc = "Commit staged changes" },
-        },
-      },
-    }
   },
 
 }
