0fe26fd13998ecba4bda2e42a57b999dc4844016
Clive Gurure <clivethescott@gmail.com>
2025-05-07 15:20:53 +0100

    updates misc

--- neovim/lua/deps/diffview.lua
+++ neovim/lua/deps/diffview.lua
@@ -4,19 +4,9 @@ return {
   keys = {
     { "<space>gz", '<cmd>DiffviewClose<cr>', desc = "Close Diffview" },
   },
-  config = {
+  opts = {
     default_args = {
       DiffviewOpen = { "--imply-local" }, -- https://github.com/sindrets/diffview.nvim/blob/main/USAGE.md#comparing-all-the-changes
     },
-    keymaps = {
-      file_panel = {
-        {
-          "n", "cc",
-          function() require 'neogit'.open({ 'commit' }) end,
-          { desc = "Commit staged changes" },
-        },
-      },
-    }
   },
-
 }
--- neovim/lua/deps/mason.lua
+++ neovim/lua/deps/mason.lua
@@ -2,18 +2,9 @@ return {
   'williamboman/mason.nvim',
   event = 'VeryLazy',
   dependencies = {
-    -- 'williamboman/mason-lspconfig.nvim',
     'neovim/nvim-lspconfig',
   },
-  config = function()
-    require('mason').setup()
-
-    -- require 'mason-lspconfig'.setup({
-    --   ensure_installed = {
-    --     'jsonls', 'lua_ls', 'ts_ls', 'gopls', 'ruff', 'pyright'
-    --   }
-    -- })
-  end,
+  config = true,
   keys = {
     { '<space>om', '<cmd>Mason<cr>', 'Mason' }
   }
