cb4855a42faa0806e316fd0f71d0903a76d76918
Clive Gurure <clivethescott@gmail.com>
2025-05-10 00:19:27 +0100

    remove flash

---
+++ upgrade.fish
@@ -0,0 +1,19 @@
+brew upgrade
+if type -q mise
+  mise upgrade
+else
+  echo 'mise is not installed....skipping update'
+end
+if type -q rustup
+  rustup update
+else
+  echo 'rustup is not installed....skipping update'
+end
+if type -q devx
+  devx update
+else
+  echo 'devx is not installed....skipping update'
+end
+if type -q nvim
+  echo "Open nvim and run :Lazy sync, :Mason update"
+end
