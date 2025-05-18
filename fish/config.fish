4a47e0d0aa29c622a19cbb77634b4f78c8b4b4d3
Clive Gurure <clivethescott@gmail.com>
2025-05-18 23:22:22 +0100

    nicer help highlights

--- fish/config.fish
+++ fish/config.fish
@@ -147,6 +147,8 @@ end
 # Requires `brew install bat`
 if type -q bat
   abbr --add -g cat 'bat'
+  abbr -a --position anywhere -- --help '--help | bat -plhelp'
+  abbr -a --position anywhere -- -h '-h | bat -plhelp'
 end
 
 if type -q ast-grep
