1477018c39a7d37853bb7136a50fcf8ea4a7d2c2
Clive Gurure <clivethescott@gmail.com>
2025-05-26 00:19:29 +0100

    wezterm types cfg

--- neovim/lua/deps/lazydev.lua
+++ neovim/lua/deps/lazydev.lua
@@ -1,12 +1,17 @@
 return {
   "folke/lazydev.nvim",
   ft = "lua", -- only load on lua files
+  dependencies = {
+    { 'gonstoll/wezterm-types', lazy = true },
+  },
   opts = {
     library = {
       -- See the configuration section for more details
       -- Load luvit types when the `vim.uv` word is found
+      "~/.config/wezterm",
       { path = "${3rd}/luv/library", words = { "vim%.uv" } },
       { path = "Snacks.nvim",        words = { "Snacks" } },
+      { path = 'wezterm-types',      mods = { 'wezterm' } },
     },
   },
 }
