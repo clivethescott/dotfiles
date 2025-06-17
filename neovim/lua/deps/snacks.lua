52babba0ed1b172597c3a59a3be32f19089683ab
Clive Gurure <clivethescott@gmail.com>
2025-06-17 16:31:55 +0100

    skip g key in dashboard for lazygit

--- neovim/lua/deps/snacks.lua
+++ neovim/lua/deps/snacks.lua
@@ -50,6 +50,18 @@ return {
       enabled = true,
       width = 70,
       sections = dashboard_sections,
+      preset = {
+        keys = {
+          { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+          { icon = " ", key = "F", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "o", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+          { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+          { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+        }
+      }
     },
     lazygit = { enabled = true },
     bigfile = { enabled = true },
