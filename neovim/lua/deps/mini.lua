4b3722752e73b6aa452d2d45ea79da21592fe946
Clive Gurure <clivethescott@gmail.com>
2025-10-16 23:27:57 +0100

    setup surround plugin

--- neovim/lua/deps/mini.lua
+++ neovim/lua/deps/mini.lua
@@ -9,12 +9,20 @@ return {
     }
   },
   {
-    'nvim-mini/mini.pairs',
-    event = 'InsertEnter',
+    'nvim-mini/mini.surround',
+    event = 'VeryLazy',
     opts = {
+      -- Module mappings. Use `''` (empty string) to disable one.
       mappings = {
-        ['`'] = { action = 'closeopen', pair = '``', neigh_pattern = '[^\\`].', register = { cr = false } },
-      }
+        add = 'sa',    -- Add surrounding in Normal and Visual modes
+        delete = 'sd', -- Delete surrounding
+        find = 'sf',   -- Find surrounding (to the right)
+        find_left = 'sF', -- Find surrounding (to the left)
+        highlight = 'sh', -- Highlight surrounding
+        replace = 'sr', -- Replace surrounding
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
     }
   },
 }
