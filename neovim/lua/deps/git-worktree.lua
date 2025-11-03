f74c798c50fde0099743eed5548b8a59490d9fea
Clive Gurure <clivethescott@gmail.com>
2025-11-03 01:24:55 +0000

    experimental run job on worktree switch

--- neovim/lua/deps/git-worktree.lua
+++ neovim/lua/deps/git-worktree.lua
@@ -2,6 +2,12 @@
 ---@field branch string The name of the branch
 ---@field path string Path to where the worktree is
 
+---@param file string
+local has_root_file = function(file)
+  local file_path = vim.fs.joinpath(vim.fn.getcwd(), file)
+  return vim.uv.fs_stat(file_path) ~= nil
+end
+
 ---@return Worktree[]
 local get_worktrees = function()
   if Snacks.git.get_root() == nil then
@@ -42,7 +48,30 @@ end
 
 return {
   'ThePrimeagen/git-worktree.nvim',
-  config = true,
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  config = function()
+    local Job = require 'plenary.job'
+    local wt = require('git-worktree')
+    wt.setup()
+
+    wt.on_tree_change(function(op, path, upstream)
+      if (op == "switch" or op == "create") and has_root_file('build.sbt') then
+        Job:new({
+          command = 'sbtn',
+          args = { 'compile' },
+          cwd = vim.fn.getcwd(),
+          env = { ['a'] = 'b' },
+          on_exit = function(j, return_val)
+            print(return_val)
+            print(j:result())
+          end,
+        }):start()
+        vim.notify('Compiling SBT project', vim.log.levels.INFO)
+      end
+    end)
+  end,
   keys = {
     { "<space>gw", select_worktree, desc = 'Switch worktree' }
   }
