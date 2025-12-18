b7b6a5e24118c8188f6167735dd27b34f92ca63c
Clive Gurure <clivethescott@gmail.com>
2025-12-18 00:21:58 +0000

    disable some snacks stuff

---
+++ neovim/lua/deps/archived/snacks.lua
@@ -0,0 +1,220 @@
+---@diagnostic disable: missing-fields
+---@diagnostic disable-next-line: param-type-mismatch
+local plugins_dir        = vim.fs.joinpath(vim.fn.stdpath('data'), '/lazy')
+local conf_dirs          = { '~/.config/nvim', '~/.config/wezterm',
+  '~/.config/atuin', '~/.config/lazygit', '~/.config/ghostty' }
+
+---@type snacks.picker.Config
+local picker_config      = {
+  enabled = vim.g.use_picker == 'snacks.picker',
+  win = {
+    input = {
+      keys = {
+        ["<a-i>"] = { "toggle_preview", mode = { "i", "n" } },
+        ["<c-v>"] = { "edit_vsplit", mode = { "i", "n" } },
+        ["<c-s>"] = { "edit_split", mode = { "i", "n" } },
+        ["<c-t>"] = { "edit_tab", mode = { "i", "n" } },
+        ["<c-q>"] = { "qflist", mode = { "i", "n" } },
+        ["<a-g>"] = { "toggle_ignored", mode = { "i", "n" } },
+        ["<a-m>"] = { "toggle_hidden", mode = { "i", "n" } },
+        ["<a-f>"] = { "toggle_maximize", mode = { "i", "n" } },
+        ["<c-g>"] = { "toggle_live", mode = { "i", "n" } },
+        ["<M-Down>"] = { "history_forward", mode = { "i", "n" } },
+        ["<M-Up>"] = { "history_back", mode = { "i", "n" } },
+        -- ["<a-f>"] = { "toggle_follow", mode = { "i", "n" } },
+        -- ["<a-h>"] = { "toggle_hidden", mode = { "i", "n" } },
+        -- ["<a-i>"] = { "toggle_ignored", mode = { "i", "n" } },
+        ["<a-r>"] = { "toggle_regex", mode = { "i", "n" } },
+        -- ["<a-m>"] = { "toggle_maximize", mode = { "i", "n" } },
+        -- ["<a-p>"] = { "toggle_preview", mode = { "i", "n" } },
+      },
+    },
+  },
+  formatters = {
+    file = {
+      filename_first = true,
+      truncate = 80,
+    },
+  },
+  layout = { hidden = { "preview" }, fullscreen = false },
+  ui_select = true,
+}
+
+local show_git           = function()
+  local git_root = Snacks.git.get_root() or ''
+  local match = string.find(git_root, 'subscription-service', 1, true) or
+      string.find(git_root, 'registry', 1, true) or
+      0
+  return match > 0
+end
+---@type snacks.dashboard.Section
+local dashboard_sections = {
+  { section = "header" },
+  { section = "keys", gap = 0.5, padding = 1 },
+  { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+  {
+    pane = 2,
+    icon = " ",
+    title = "Git Status",
+    section = "terminal",
+    enabled = show_git,
+    cmd = "git status --short --branch --renames",
+    height = 3,
+    padding = 1,
+    ttl = 5 * 60,
+    indent = 3,
+  },
+  {
+    pane = 1,
+    icon = " ",
+    section = "terminal",
+    title = "Open PRs",
+    cmd = 'gh pr list -L 5 --author "@me"',
+    key = "P",
+    enabled = show_git,
+    ttl = 1 * 60,
+    action = function()
+      vim.fn.jobstart('gh pr list --author "@me" --web', { detach = true })
+    end,
+    height = 10,
+    width = 150,
+  },
+  { section = "startup" },
+}
+
+local dashboard_keys     = {
+  { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+  { icon = " ", key = "F", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+  { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+  { icon = " ", key = "o", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+  { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+  { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+  { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+  { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+}
+
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    dashboard = {
+      enabled = true,
+      width = 70,
+      sections = dashboard_sections,
+      preset = {
+        keys = dashboard_keys,
+      }
+    },
+    lazygit = {
+      enabled = true,
+      config = {
+        os = {
+          editPreset = "nvim-remote",
+          -- https://github.com/jesseduffield/lazygit/blob/master/docs/Config.md#configuring-file-editing
+          edit =
+          '`begin; if test -z "$NVIM"; nvim -- {{filename}}; else; nvim --server "$NVIM" --remote-send "q"; nvim --server "$NVIM" --remote {{filename}}; end; end`',
+          editAtLine =
+          '`begin; if test -z "$NVIM"; nvim +{{line}} -- {{filename}}; else; nvim --server "$NVIM" --remote-send "q"; nvim --server "$NVIM" --remote {{filename}}; nvim --server "$NVIM" --remote-send ":{{line}}<CR>"; end; end`',
+        },
+      }
+    },
+    statuscolumn = { enabled = true },
+    bigfile = { enabled = false },
+    notifier = { enabled = true },
+    rename = { enabled = true },
+    image = { enabled = true },
+    picker = picker_config,
+    gh = { enabled = true },
+    scratch = {
+      enabled = true,
+      filekey = { branch = false },
+      wo = { relativenumber = true }
+    },
+  },
+  keys = {
+    { "gs",        function() require 'snacks'.lazygit.open() end,          desc = "Lazygit" },
+    { "<space>ts", function() Snacks.scratch() end,                         desc = "Toggle scratch buffer" },
+    { "<space>tS", function() Snacks.scratch.select() end,                  desc = "Select scratch buffer" },
+    { "<space>gp", function() Snacks.picker.gh_pr() end,                    desc = "GitHub Open PRs" },
+    { "<space>gP", function() Snacks.picker.gh_pr({ state = "all" }) end,   desc = "GitHub All PRs" },
+    { "<space>nh", function() require 'snacks'.notifier.show_history() end, desc = "Notification history" },
+    { "<space>gl", function() Snacks.picker.git_log_line() end,             desc = "Line Commits" },
+    { "<space>gL", function() Snacks.picker.git_log_file() end,             desc = "Buffer Commits" },
+    { "<space>gb", function() Snacks.picker.git_branches() end,             desc = "Git Branches" },
+    { "<c-p>",     function() Snacks.picker.files() end,                    desc = "Files" },
+    { "<M-p>",     function() Snacks.picker.smart() end,                    desc = "Smart" },
+    { "<c-e>",     function() Snacks.picker.buffers() end,                  desc = "Buffers" },
+    { "<space>tf", function() Snacks.picker.grep() end,                     desc = "Grep" },
+    { "<space>tF", function() Snacks.picker.grep_word() end,                desc = "Grep selection or word", mode = { "n", "x" } },
+    { "<space>to", function() Snacks.picker.recent() end,                   desc = "Recent Files" },
+    { "<space>tq", function() Snacks.picker.qflist() end,                   desc = "Quickfix List" },
+    { "<space>tl", function() Snacks.picker.resume() end,                   desc = "Resume" },
+    { "<space>tc", function() Snacks.picker.commands() end,                 desc = "Commands" },
+    { "<space>tw", function() Snacks.picker.spelling() end,                 desc = "Spelling" },
+    { "<space>tC", function() Snacks.picker.command_history() end,          desc = "Command history" },
+    { "<space>th", function() Snacks.picker.help() end,                     desc = "Help tags" },
+    { "<space>tH", function() Snacks.picker.highlights() end,               desc = "Highlights" },
+    { "<space>tm", function() Snacks.picker.marks() end,                    desc = "Marks" },
+    { "<space>tj", function() Snacks.picker.jumps() end,                    desc = "Jumps" },
+    { "<space>tz", function() Snacks.picker.zoxide() end,                   desc = "Zoxide" },
+    { "<space>tt", function() Snacks.picker() end,                          desc = "Pickers" },
+    ---@diagnostic disable-next-line: undefined-field
+    { "<space>tu", function() Snacks.picker.undo() end,                     desc = "Undo" },
+    { "<space>tr", function() Snacks.picker.registers() end,                desc = "Registers" },
+    { "<space>tb", function() Snacks.picker.lines() end,                    desc = "Buffer lines" },
+    -- { "<space>lm",  function() Snacks.picker.lsp_definitions() end,      desc = "Goto Definition" },
+    { "<space>lD", function() Snacks.picker.diagnostics() end,              desc = "Snacks Diagnostics" },
+    { "<space>lr", function() Snacks.picker.lsp_references() end,           nowait = true,                   desc = "References" },
+    { "<space>lI", function() Snacks.picker.lsp_implementations() end,      desc = "Goto Implementation" },
+    { "<space>ly", function() Snacks.picker.lsp_type_definitions() end,     desc = "Goto T[y]pe Definition" },
+    { "<space>lW", function() Snacks.picker.lsp_workspace_symbols() end,    desc = "LSP Workspace Symbols" },
+    { "<space>lw", function() Snacks.picker.lsp_symbols() end,              desc = "LSP Project Symbols" },
+    {
+      "<space>ho",
+      function() Snacks.picker.files { dir = '~/Code/HTTP/' } end,
+      desc = "HTTP files"
+    },
+    {
+      '<space>te',
+      function()
+        Snacks.explorer.open {
+          diagnostics = false,
+          supports_live = true,
+        }
+      end,
+      desc = 'Toggle explorer'
+    },
+    {
+      '<space>tE',
+      function()
+        Snacks.explorer.open {
+          follow_file = true,
+          diagnostics = false,
+        }
+      end,
+      desc = 'Select file in explorer'
+    },
+    {
+      "<leader>2",
+      function() Snacks.picker.files { dirs = conf_dirs } end,
+      desc = "Open Config files"
+    },
+    {
+      "<leader>4",
+      function() Snacks.picker.files { dirs = { plugins_dir } } end,
+      desc = "Open Plugin files"
+    },
+    {
+      "<leader>5",
+      function() Snacks.picker.grep { dirs = { plugins_dir } } end,
+      desc = "Grep Plugin files"
+    },
+    {
+      "<leader>3",
+      function() Snacks.picker.grep { dirs = conf_dirs } end,
+      desc = "Grep config"
+    },
+  }
+}
