3a2942efe4992ddfff06d9ffcfe526aadde7f424
Clive Gurure <clivethescott@gmail.com>
2026-02-11 16:36:33 +0000

    rm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -99,7 +99,6 @@ return {
     saturation = 0.2,
     brightness = 0.2,
   },
-  send_composed_key_when_left_alt_is_pressed = true, --fix Option+3 -> # issue
   -- unix_domains = {
   --   { name = 'remote' }
   -- },
