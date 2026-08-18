c3b80843655413ded5dff8efa1f02af19225ea98
Clive Gurure <clivethescott@gmail.com>
2026-08-18 02:08:16 +0100

    change say keybind

--- neovim/plugin/say.lua
+++ neovim/plugin/say.lua
@@ -18,11 +18,11 @@ local function toggle(text)
   speak(text)
 end
 
-vim.keymap.set('n', '<leader>s', function()
+vim.keymap.set('n', '<leader>S', function()
   toggle(table.concat(vim.api.nvim_buf_get_lines(0, 0, -1, false), '\n'))
 end, { desc = 'Say: toggle speak whole buffer' })
 
-vim.keymap.set('x', '<leader>s', function()
+vim.keymap.set('x', '<leader>S', function()
   local text = table.concat(
     vim.fn.getregion(vim.fn.getpos('v'), vim.fn.getpos('.'), { type = vim.fn.mode() }),
     '\n')
