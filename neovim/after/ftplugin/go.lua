1810b93ee4e9a2f6182980ba6b481a18725eb40a
Clive Gurure <clivethescott@gmail.com>
2026-09-09 00:09:02 +0100

    isc

--- neovim/after/ftplugin/go.lua
+++ neovim/after/ftplugin/go.lua
@@ -1,1 +1,3 @@
 vim.keymap.set({ 'n', 'i', 'v' }, '<c-b>', ':2TermExec cmd="go vet" direction=tab', { desc = "Go vet" })
+-- https://go.dev/gopls/editor/vim#a-hrefsemantic-tokens-shadowing-idsemantic-tokens-shadowingshadowing-semantic-modifiera
+vim.api.nvim_set_hl(0, '@lsp.mod.shadowing', { bold = true, underline = true })
